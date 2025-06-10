0889c37116d84336e8eb3bfe59fdffc39ca61464
sammycaleb <mutugisammycaleb@gmail.com>
2025-06-10 12:30:51 +0300

    bank_account class

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,17 @@
+class BankAccount:
+  def __init__(self, initial_balance = 0):
+    self.__account_balance = initial_balance
+
+  def deposit(self, amount):
+    if amount > 0:
+      self.__account_balance += amount
+    else:
+      print("deposit must be a positive amount")
+  def withdraw(self, amount):
+    if amount < self.__account_balance:
+      self.__account_balance -= amount
+      return True
+    else:
+      print("insufficient funds.")
+  def display_balance(self):
+    print(f"current balance: {self.__account_balance}")
